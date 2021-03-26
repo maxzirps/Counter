01655bf6608542190f6a0be8dde9c04ff5f9dc28
Maximilian Zirps <29249513+maxzirps@users.noreply.github.com>
2021-03-26 22:32:45 +0100

    Initial Commit

---
+++ Shared/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Shared
+//
+//  Created by Maximilian Zirps on 26.03.21.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
